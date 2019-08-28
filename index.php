ca4a27345d179f85a1362fd4f55af491f333a7b8
mcvaques <mariaclaravasques99@gmail.com>
2019-08-28 00:22:51 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+var_dump("a");
+die
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>KINGBURGUER</title>
+        <meta charset="utf8"></meta>
+        <link rel="stylesheet" type="text/css" href="../css/estilo.css">
+    </head>
+
+    <body>
+        <h1>PEDIDOS</h1>
+            <form action="" method="get"></form>
+            <label for="Nome">Nome do Pedido</label>
+            <input id="name" type="text" placeholder="Ex: X-tudo" name="nome"> 
+            <input type="submit" valeu="ENVIA">
+           <p>
+            <h2>SELECIONE A QUANTIDADE:</h2>
+              <p>Pão</p>
+               <select name="select">
+                <option value="1">1</option> 
+                <option value="2" selected>2</option>
+                <option value="3"> 3</option> 
+                <input type="submit" value="Enviar">
+              </select>
+            <p>Carne</p>
+             <select name="select">
+              <option value="1">1</option> 
+              <option value="2" selected>2</option>
+              <option value="3"> 3</option> 
+              <input type="submit" value="Enviar">
+            </select>
+            <p>Salada</p>
+            <select name="select">
+             <option value="1">1</option> 
+             <option value="2" selected>2</option>
+             <option value="3"> 3</option> 
+             <input type="submit" value="Enviar">
+           </select>
+           <p>Queijo</p>
+           <select name="select">
+            <option value="1">1</option> 
+            <option value="2" selected>2</option>
+            <option value="3"> 3</option> 
+            <input type="submit" value="Enviar">
+          </select>
+          <p>Ovos</p>
+          <select name="select">
+           <option value="1">1</option> 
+           <option value="2" selected>2</option>
+           <option value="3"> 3</option> 
+           <input type="submit" value="Enviar">
+         </select>
+         <p>Batata Palha</p>
+         <select name="select">
+          <option value="1">1</option> 
+          <option value="2" selected>2</option>
+          <option value="3"> 3</option> 
+          <input type="submit" value="Enviar">
+        </select>
+        <p>Milho</p>
+          <select name="select">
+           <option value="1">1</option> 
+           <option value="2" selected>2</option>
+           <option value="3"> 3</option> 
+           <input type="submit" value="Enviar">
+         </select>
+
+</body>
+</html>
+
