94e3d5908fce18f0218377eb62c03d45f903a0fb
mcvaques <mariaclaravasques99@gmail.com>
2019-08-30 00:59:44 -0300

    formulario finalizado, falta pequenos ajustes no css

--- index.php
+++ index.php
@@ -1,74 +1,99 @@
 <!DOCTYPE html>
 
-<html>
+<html lag="pt-br">
     <head>
-        <title>KINGBURGER</title>
+        <title>QUEENBURGER</title>
         <meta charset="utf8"> 
-        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+        <link rel="stylesheet" href="css/normalize">
+        <link rel="stylesheet" type="text/css" href=../css/estilo.css>
     </head>
 
     <body> 
-      <div class="principal">
-        <h1>PEDIDOS QUEENBURGER</h1>
+      <div id="principal">
+      <h1>PEDIDOS QUEENBURGER</h1>
 
-               <h2>SELECIONE A QUANTIDADE:</h2>
-              <p>Pão</p>
-                <input name="name" type="number" value="99" min="0">
-              <p>Carne</p>
-               <input name=name type="number" valeu="99" min="0">
-              
-              <p>Salada</p>
-              <input name=name type="number" valeu="99" min="0">
-             
-             <p>Queijo</p>
-             <input name=name type="number" valeu="99" min="0">
-
-             <p>Ovos</p>
-             <input name=name type="number" valeu="99" min="0"> 
-
-             <p>Bacon</p>
-             <input name=name type="number" valeu="99" min="0"> 
-
-          <h2>Salada</h2>
-           <input type="checkbox" name="Alface"> Alface
-           <input type="checkbox" name="Tomate"> Tomate
-           <input type="checkbox" name="Batata Palha"> Batata Palha
-           
-           <h2>Molhos</h2>
-           <input type="checkbox" name="Cheddar"> Cheddar
-           <input type="checkbox" name="Catupiry">Catupiry
-           <input type="checkbox" name="Maionese Temperada">Maionese Temperada 
-           <input type="checkbox" name="Ketchup">Ketchup
-           <h2>Tipos de Sanduíches</h2>
+      <h2>Selecione o Sanduíche</h2>
            <input type="radio" name="name" value="Hamburger">Hamburguer
            <input type="radio" name="name" value="CheeseBurger">CheeseBurger
            <input type="radio" name="name" value="EggcheeseBurger">EggcheeseBurger
            <input type="radio" name="name" value="CheeseBacon">CheeseBacon
            <input type="radio" name="name" value="EggCheeseBacon">EggCheeseBacon
+
+      <h2>Tipo da Carne</h2>
+      <input type="radio" name=name valeu="simples">Simples
+      <input type="radio" name= name valeu="picanha">Picanha
            
-           <h2>Refrigerantes</h2>
+      <h2>Selecione a Quantidade:</h2>
+      <p>Pão</p>
+          <input name=name type=number valeu="99" min="0">
+      <p>Carne<p>
+          <input name=name type="number" valeu="99" min="0">
+      
+      <p>Queijo</p>
+          <input name=name type="number" valeu="99" min="0">
+
+      <p>Ovos</p>
+          <input name=name type="number" valeu="99" min="0"> 
+
+      <p>Bacon</p>
+          <input name=name type="number" valeu="99" min="0"> 
+
+      <h2>Selecione a Salada</h2>
+      <input class="checkbox" type="checkbox"> 
+           <input type="checkbox" name="Alface"> Alface
+           <input type="checkbox" name="Tomate"> Tomate
+           <input type="checkbox" name="Batata Palha"> Batata Palha
+           
+      <h2> Selecione os Molhos</h2>
+           <input type="checkbox" name="Cheddar"> Cheddar
+           <input type="checkbox" name="Catupiry">Catupiry
+           <input type="checkbox" name="Maionese Temperada">Maionese Temperada 
+           <input type="checkbox" name="Ketchup">Ketchup
+      
+           <h2>Turbinar o lanche<h2>
+            <p>Carne Simples</p>
+            <input name=name type="number" value="99" min="0">
+            
+            <p>Carne de Picanha</p>
+            <input name=name type="number" value="99" min="0">
+
+            <p>Bacon</p>
+            <input name=name type="number" value="99" min="0">
+
+            <p>Queijo</p>
+            <input name=name type="number" value="99" min="0">
+
+            <p>Ovo</p>
+            <input name=name type="number" value="99" min="0">
+
+            <p>Maionese Temperada</p>
+            <input name=name type=number value="99" min="0">
+
+      <h2>Combos</h2>
+        <p>Sanduíche+Batata Tubinada</p>
+        <input name=name type="number" valeu="99" min="0">
+        <label>Tipo Da Carne</label>
+        <input type="radio" name=name valeu="simples">Simples
+        <input type="radio" name= name valeu="picanha">Picanha
+      
+      <h2>Refrigerantes</h2>
            <input type="checkbox" name="CocaCola">CocaCola
            <input type="checkbox" name="Guarana Kuat">Guaraná Kuat
            <input type="checkbox" name="Guarana Natural">Guaraná Natural 
-           <h3>Tipos:</h3>
+      <h3>Tipos:</h3>
            <input type="radio" name="name" value="Lata">Lata
            <input type="radio" name="name" value="2Litros">2 Litros 
 
-           <h3>Quantidade:</h3>
+      <h3>Quantidade:</h3>
            <input name=name type="number" valeu="99" min="0"> 
 
            <input type="submit" value="Finalizar Pedido">
 
-                
-
-         
-        
-
+              
 </div>
 
 </body>
 </html>
-
 
      
      
